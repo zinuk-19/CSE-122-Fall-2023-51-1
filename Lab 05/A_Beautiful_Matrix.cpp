6993a6276c7f8394cfb2f08e80faa55d0c422d85
zinuk <139265663+zinuk-19@users.noreply.github.com>
2023-08-08 15:19:28 +0600

    Create A_Beautiful_Matrix.cpp

---
+++ Lab 05/A_Beautiful_Matrix.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+ 
+using namespace std;
+ 
+int main()
+ 
+{
+    
+    int x;
+ 
+    for (int i = 0; i < 5; i++)
+ 
+    {
+ 
+        for (int j = 0; j < 5; j++)
+ 
+        {
+            cin >> x;
+ 
+            if (x == 1)
+ 
+            {
+ 
+                cout << abs(i - 2) + abs(j - 2) << "\n";
+ 
+            }
+ 
+        }
+        
+    }
+
+    return 0;
+ 
+}
